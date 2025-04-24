b59266bc7a568190b1e097603743259cdff2e814
Arjun Mahishi <arjun.mahishi@gmail.com>
2025-04-24 12:26:25 +0530

    feat: Add support for CONFIG_PATH env var in run.sh
    Added ability to pass config file path via environment variable to the Docker container.

--- scripts/run.sh
+++ scripts/run.sh
@@ -17,12 +17,19 @@ getEnv() {
 
 ADDR=$(getEnv "REMOTE_WRITE_ADDR")
 FREQ=$(getEnv "FREQUENCY" "5s")
+CONFIG=$(getEnv "CONFIG_PATH")
 
 echo "Starting randOME with frequency $FREQ"
 
+CONFIG_ARG=""
+if [ -n "$CONFIG" ]; then
+    echo "Using config file: $CONFIG"
+    CONFIG_ARG="-c $CONFIG"
+fi
+
 if [ -n "$ADDR" ]; then
     echo "Remote write address: $ADDR"
-    /app/randOME remote-write -f $FREQ --addr $ADDR
+    /app/randOME remote-write -f $FREQ --addr "$ADDR" $CONFIG_ARG
 else
-    /app/randOME emit -f $FREQ 
+    /app/randOME emit -f $FREQ $CONFIG_ARG
 fi
